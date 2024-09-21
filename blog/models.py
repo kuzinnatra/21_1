8f0f50ac3521c18190de615cc15090eb684ef2ee
kuzin_sn <kuzinnatra@yandex.ru>
2024-09-21 18:21:10 +0400

    add slug для заголовка

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,10 @@ class Blog(models.Model):
     title = models.CharField(max_length=150, verbose_name='название')
     body = models.TextField(verbose_name='содержимое')
 
+    views_count = models.IntegerField(default=0, verbose_name='просмотры')
+    is_published = models.BooleanField(default=True, verbose_name='опубликовано')
+    slug = models.CharField(max_length=150, verbose_name='slug',null=True, blank=True)
+
     # заголовок;
     # slug(реализоватьчерезCharField);
     # содержимое;

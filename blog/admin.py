df1c7be2c7f270800dedfce40175e8553300aeb8
kuzin_sn <kuzinnatra@yandex.ru>
2024-09-21 21:34:13 +0400

    реализована админка blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from blog.models import Blog
+
+
 # Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_create', 'is_published', 'views_count',)
+    list_filter = ('is_published',)

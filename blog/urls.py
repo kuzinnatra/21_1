cd76881d0dfb37b25d3c4ab34ceae50edbf94139
kuzin_sn <kuzinnatra@yandex.ru>
2024-09-21 15:14:48 +0400

    создана заготовка страницы create

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from blog.apps import BlogConfig
+from blog.views import BlogCreateView
 from catalog.views import ProductListView, contacts, ProductDetailView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('create/', ..., name='create'),
-    path('', ..., name='list'),
-    path('view/<int:pk>/', ..., name='view'),
-    path('edit/<int:pk>/', ..., name='edit'),
-    path('delete/,<int:pk>/', ..., name='delete'),
+    path('create/', BlogCreateView.as_view(), name='create'),
+    # path('', ..., name='list'),
+    # path('view/<int:pk>/', ..., name='view'),
+    # path('edit/<int:pk>/', ..., name='edit'),
+    # path('delete/,<int:pk>/', ..., name='delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+
+from blog.models import Blog
+
 
 # Create your views here.
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('title', 'body',)

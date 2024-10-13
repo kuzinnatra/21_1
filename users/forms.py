d43635172ce07aff1a3fbba6b759163b9aa22c8f
kuzin_sn <kuzinnatra@yandex.ru>
2024-10-13 15:21:50 +0400

    Выполнено задание2 22_2

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,20 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
 
+from django import forms
 from catalog.forms import StyleFormMixin
 from users.models import User
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class UserRegisterForm(StyleFormMixin, UserCreationForm):
     class Meta:
         model = User
         fields = ("email", "password1", "password2")
+
+
+class ResetPasswordForm(StyleFormMixin, PasswordResetForm):
+    """Форма для сброса пароля"""
+    class Meta:
+        model = User
+        fields = ['email', ]
 
--- users/views.py
+++ users/views.py
@@ -1,11 +1,86 @@
-from django.urls import reverse_lazy
+import random
+import secrets
+import string
+
+from django.contrib.auth.views import PasswordResetView
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
 
-from users.forms import UserRegisterForm
+from catalog.forms import StyleFormMixin
+from users.forms import UserRegisterForm, ResetPasswordForm
 from users.models import User
+from config.settings import EMAIL_HOST_USER
 
 
 class UserCreateView(CreateView):
+    """
+    Регистрация нового пользователя
+    """
     model = User
     form_class = UserRegisterForm
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        """
+        Создаем токен
+        """
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f'http://{host}/users/email-confirm/{token}/'
+        send_mail(
+            subject="Подтверждение почты",
+            message=f"Перейдите по ссылке для подтверждения почты {url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        return super().form_valid(form)
+
+
+def email_verification(request, token):
+    """
+    Подтверждение почты с использованием токена
+    """
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse("users:login"))
+
+
+class UserResetPasswordView(PasswordResetView, StyleFormMixin):
+    """
+    Сброс пароля с использованием email
+    """
+    form_class = ResetPasswordForm
+    template_name = 'users/password-reset.html'
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        """
+        Проверяет валидность и сохраняет новый пароль
+        """
+        email = form.cleaned_data['email']
+        # Проверяем наличие пользователя с указанной почтой
+        try:
+            user = User.objects.get(email=email)
+            if user:
+                # Создаем новый пароль для пользователя и отправляем его на почту
+                password = ''.join([random.choice(string.digits + string.ascii_letters) for i in range(0, 10)])
+                user.set_password(password)
+                user.is_active = True
+                user.save()
+                send_mail(
+                    subject='Сброс пароля',
+                    message=f'Ваш новый пароль: {password}',
+                    from_email=EMAIL_HOST_USER,
+                    recipient_list=[user.email]
+                )
+            return redirect(reverse('users:login'))
+        except User.DoesNotExist:
+            # Если пользователь не найден, перенаправляем на страницу регистрации
+            return redirect(reverse('users:register'))
